fd54bd684e3a9e2192c9f2da87c730fcc3e30770
khypnos <khypnos.med@gmail.com>
2025-10-13 01:35:23 +0200

    Add files via upload

--- js/header-manager.js
+++ js/header-manager.js
@@ -1,11 +1,10 @@
-<script>
+
 document.addEventListener('DOMContentLoaded', function () {
 	const headerHtml = `
 <header class="headerbis">
-	<h1>🩺 Khypnos</h1>
+	<h1><img src="/img/Y_Khypnos.png" alt="Khypnos" class="logo"> Khypnos</h1>
 	<h2>Préparer les EVC en Anesthésie Réanimation</h2>
 	<div class="subtitle">Méthodologie • Annales • Recommandations</div>
 </header>`;
 	document.body.insertAdjacentHTML('afterbegin', headerHtml);
 });
-</script>
